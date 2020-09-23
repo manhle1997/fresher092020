dde6dc70fd8d6f5077ded93df7faaade6f539457
MANH LE QUOC <lqmanh@rd.local>
2020-09-23 11:57:02 +0700

    complete form base without dialog

---
+++ MISA.CukCuk/MISA.CukCuk/wwwroot/js/lib/dialog.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(".btn-close").click(function () {
+        $(".form-dialog").hide();
+    });
+});
+$(document).ready(function () {
+    $("#toolbar-item-add").click(function () {
+        $(".form-dialog").show();
+    });
+});
