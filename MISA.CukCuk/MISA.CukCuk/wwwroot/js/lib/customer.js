329db85ec46b64bfcf94101872744ff1b5a8bd80
manhle1997 <lqmanhddt@gmail.com>
2020-09-30 07:07:34 +0700

    add Add method

--- MISA.CukCuk/MISA.CukCuk/wwwroot/js/lib/customer.js
+++ MISA.CukCuk/MISA.CukCuk/wwwroot/js/lib/customer.js
@@ -11,9 +11,41 @@ $(document).ready(function () {
 class CustomerJS extends BaseJS {
     constructor() {
         super();
+        this.addData();
     }
     getData() {
         this.Data = data;
+    }
+    addData() {
+        $('#btn_save').click(function () {
+            //Lấy dữ liệu nhập từ input
+            var customerCode = $('#customer-code').val();
+            var customerName = $('#customer-name').val();
+            var customerCompany = $('#customer-company').val();
+            var customerTaxCode = $('#customer-taxcode').val();
+            var customerAddress = $('#customer-address').val();
+            var customerPhonenumber = $('#customer-phonenumber').val();
+            var customerEmail = $('#customer-email').val();
+
+            //tạo đối tượng và lưu thông tin vào mảng data
+            var customer = {
+                CustomerCode: customerCode,
+                CustomerName: customerName,
+                CustomerCompany: customerCompany,
+                TaxCode: customerTaxCode,
+                Address: customerAddress,
+                PhoneNumber: customerPhonenumber,
+                Email: customerEmail,
+                Salary: 20000000
+            }
+
+            data.push(customer);
+            
+            //thông báo
+            alert("Lưu thông tin thành công");
+            this.getData();
+            this.loadData();
+        }.bind(this));
     }
 
 }
