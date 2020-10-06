1c94c9b917397a7d4e80bd6abad20f1a77ddcd1e
MANH LE QUOC <lqmanh@rd.local>
2020-10-06 11:16:04 +0700

    doing merge

--- MISA.CukCuk/MISA.CukCuk/wwwroot/js/base/base.js
+++ MISA.CukCuk/MISA.CukCuk/wwwroot/js/base/base.js
@@ -1,15 +1,8 @@
 ﻿$(document).ready(function () {
     //load dữ liệu
-    try {
-        var baseJS = new BaseJS();
-    } catch (e) {
-        console.log('fail from base.js');
-    }
+
 })
 
-/**
- * class quản lí các function cho trang customer
- * */
 
 class BaseJS {
 
@@ -17,144 +10,244 @@ class BaseJS {
         this.getData();
         this.loadData();
         this.initEvents();
-        
     }
 
     /**
-     * Hàm tạo sự kiện 
+     * Hàm tạo sự kiện cho button
      * */
-
     initEvents() {
+        var self = this;
         $("#toolbar-item-add").click(function () {
+            self.FormMode = 'Add';
+            $(".form-dialog input").val('');
+            $(".form-dialog input").removeClass('not-required')
             $(".form-dialog").show();
         });
+        $('#toolbar-item-edit').click(this.btnEditOnClick.bind(this));
+        $('#toolbar-item-delete').click(this.btnDeleteOnClick.bind(this))
+        $('#btn_save').click(this.btnSaveOnClick.bind(this))
         $(".btn-close, #btn_close").click(function () {
             $(".form-dialog").hide();
         });
         $('#tbCustomer tbody').on('click', 'tr', this, this.rowOnClick);
 
         $('#toolbar-item-reload').click(this.btnReloadDataOnClick.bind(this));
-
-        
-        
+        $('input[required]').blur(this.validateRequired);
     }
 
 
+    //#region Validate
     /**
-     * Load dữ liệu 
+     * Validate bắt buộc nhập
+     * Author: lê Mạnh
      * */
-    loadData() {
-        //lấy dữ liệu về
-        //try {
-        //    var fields = $('#tbCustomer thead .thead td');
-        //    var data = this.Data;
-        //    var self = this;
-        //    $.each(data, function (index, obj) {
-        //        var tr = $(`<tr></tr>`);
-        //        $.each(fields, function (index, field){
-        //            var fieldName = $(field).attr('fieldName');
-        //            var value = obj[fieldName];
-        //            var td = $(`<td>` + value + `</td>`);
-        //            $(tr).append(td);
-        //        })
+    validateRequired(sender) {
+        //lấy dữ liệu đã nhập\
 
-        //        //var trHTML = self.makeTrHTML(obj);
+        validData.validateRequired(sender.currentTarget);
 
-        //        $("#tbCustomer tbody").append(tr);
-        //    })
-        //}
-        //catch (e) {
-        //    console.log('error from load data');
-        //}
+        //nếu chưa nhập thì set border màudor
+    }
+    //#endregion
 
 
-        try {
-            var data = this.Data;
-            var self = this;
-            var fields = $('#tbCustomer thead .thead td')
-            $.each(data, function (index, obj) {
-                var tr = $(`<tr></tr>`);
-                $.each(fields, function (index, field) {
-                    var fieldName = $(field).attr('fieldName');
-                    var fieldNumber = $(field).attr('fieldNumber');
-                    var value = obj[fieldName];
-                    if (fieldNumber == 'Salary') {
-                        var td = $(`<td style="text-align: right;">` + commonJS.formatMoney(value) + `</td>`);
-                    } else if (fieldNumber == 'DateOfBirth') {
-                        var td = $(`<td>` + commonJS.formatDate(value) + `</td>`);
-                    }else {
-                        var td = $(`<td>` + value + `</td>`);
+    /**
+     * Hàm xóa dữ liệu được chọn
+     * Author: Lê Mạnh
+     * */
+    btnDeleteOnClick() {
+        self = this;
+        //Lấy id của bản ghi được chọn
+        var id = this.getRecordIdSelected();
+        if (id == null) {//Nếu không có Id thì in ra thông báo
+            alert('Vui lòng chọn khách hàng muốn xóa');
+        }
+        else {
+            // Hiển thị thông báo xác nhận xóa
+            var result = confirm("Bạn có muốn xóa khách hàng này");
+            if (result) {
+                var recordSelected = $('#tbCustomer tbody tr.row-selected');
+
+                // Lấy dữ liệu chi tiết bản ghi đã chọn
+                var id = recordSelected.data('keyId');
+                var objectDetail = recordSelected.data('data');
+                $.each(data, function (index, obj) {
+                    if (obj.CustomerCode == objectDetail.CustomerCode) {//nếu id của record được chọn trùng với id nào của data thì xoá object đó
+                        data.splice(index, 1);
                     }
-                    
-                    $(tr).append(td);
-                })
-                $("#tbCustomer tbody").append(tr);
+                    self.loadData();
+                });
+            }
+        }
+    }
+
+    /**
+    * Thực hiên lưu dữ liệu
+    * Author: Lê Mạnh
+    * */
+    btnSaveOnClick() {
+        try {
+            self = this;
+
+            //Validate dữ liệu
+            //Check bắt buộc nhập
+            var isValid = true;
+            var inputRequireds = $('input[required]');
+            $.each(inputRequireds, function (index, input) {
+                if (!validData.validateRequired(input)) {
+                    isValid = false;
+                }
             })
+            //Nếu valid thì gán cho 1 object
+            if (isValid) {
+                // Lấy ra các input có các trường là fieldName
+                var inputs = $('input[fieldName]');
+                //build object dữ liệu
+                var customer = {};
+                $.each(inputs, function (index, input) {
+                    var fieldName = $(input).attr('fieldName');
+                    var value = $(input).val();
+                    customer[fieldName] = value;
+                });
+                if (self.FormMode == 'Add') {
+                    alert('Thêm dữ liệu thành công');
+                    data.push(customer);
+                }
+                else {
+                    alert('Chỉnh sửa dữ liệu thành công');
+                    $.each(data, function (index, obj) {
+                        if (obj.CustomerCode == customer.CustomerCode) {
+                            data.splice(index, 1, customer);
+                        }
+                    });
+                }
+
+                self.Data = data;
+                //Load lại data         
+                self.loadData();
+                $(".form-dialog").hide();
+                return;
+            }
+            //gọi service thực hiện lưu dữ liệu
         } catch (e) {
 
         }
     }
 
     /**
-     * hàm lấy dữ liệu
+    * Hàm Edit thông tin
+    * Author: Lê Mạnh
+    */
+
+    //TODO : đang làm dở edit infor
+    btnEditOnClick() {
+        try {
+            //Lấy id của bản ghi được chọn
+            var id = this.getRecordIdSelected();
+            self = this;
+            if (id == null) {//Nếu không có Id thì in ra thông báo
+                alert('Vui lòng chọn khách hàng muốn chỉnh sửa thông tin');
+            }
+            else {
+                this.FormMode = 'Edit';
+                // Lấy thông tin bản ghi đã chọn trong danh sách
+                var recordSelected = $('#tbCustomer tbody tr.row-selected');
+
+                // Lấy dữ liệu chi tiết bản ghi đã chọn
+                var id = recordSelected.data('keyId');
+                var objectDetail = recordSelected.data('data');
+                debugger;
+
+                // Binding dữ liệu vào các input tương ứng trên form chi tiết
+                //Load tất cả input trong dialog, với mỗi input gán cho giá trị tương ứng trong objectDetail
+                var inputs = $('.dialog input');
+                $.each(inputs, function (index, input) {
+                    var fieldName = $(input).attr('fieldName');
+                    input.value = objectDetail[fieldName];
+                });
+                // Hiển thị dialog chi tiết
+                $(".form-dialog").show();
+            }
+        } catch (e) {
+
+        }
+    }
+
+
+    /**
+     * Load dữ liệu 
+     * Author: Lê Mạnh
+     * */
+    loadData() {
+        try {
+            $("#tbCustomer tbody").empty();
+            var data2 = this.Data;
+            var self = this;
+            var fields = $('#tbCustomer thead .thead td')
+            var keyId = $('#tbCustomer').attr('keyId');
+            $.each(data2, function (index, obj) {
+                var tr = $(`<tr></tr>`);
+                $.each(fields, function (index, field) {
+                    var fieldName = $(field).attr('fieldName');
+                    var fieldNumber = $(field).attr('fieldNumber');
+
+                    var value = obj[fieldName];
+                    if (fieldNumber == 'DebitMoney' || fieldNumber == 'Salary') {
+                        var td = $(`<td title="` + value + `" style="text-align: right;">` + commonJS.formatMoney(value) + `</td>`);
+                    } else if (fieldNumber == 'DateOfBirth') {
+                        var td = $(`<td title="` + value + `">` + commonJS.formatDate(value) + `</td>`);
+                    } else {
+                        var td = $(`<td title="` + value + `">` + value + `</td>`);
+                    }
+                    $(tr).data('keyId', obj[keyId]);//Thêm data 'keyId' có giá trị là obj.CustomerId vào 'tr'
+                    $(tr).data('data', obj); //Thêm data 'data' có giá trị là obj vào 'tr'
+                    $(tr).append(td);
+
+
+                });
+
+                $("#tbCustomer tbody").append(tr);
+            });
+        } catch (e) {
+
+        }
+    }
+
+    /**
+     * Hàm lấy dữ liệu
+     * Author: Lê Mạnh
      * */
     getData() {
         this.Data = [];
     }
 
-    getChoseData() {
-
-    }
-
-    /**
-     * Build Html for Tr
-     * */
-    makeTrHTML(obj) {
-
-    }
-
-    //TODO: btn ReloadData
     btnReloadDataOnClick() {
         this.loadData();
     }
 
-    //TODO: can sua
+
+
+    /**
+     * Đổi màu hàng được chọn
+     * Author: Lê Mạnh
+     * */
     rowOnClick() {
         $(this).siblings().removeClass('row-selected')
         $(this).addClass('row-selected');
     }
 
 
+
+    /**
+     * Lấy Id của bản ghi được chọn trong danh sách
+     * */
+    getRecordIdSelected() {
+        // Lấy thông tin bản ghi đã chọn trong danh sách
+        var recordSelected = $('#tbCustomer tbody tr.row-selected');
+
+        // Lấy dữ liệu chi tiết bản ghi đã chọn
+        var id = recordSelected.data('keyId');
+        return id;
+    }
 }
 
-
-//var data = [
-//    {
-//        customerCode: "KH000003",
-//        customerName: "Lê Quốc Mạnh",
-//        customerCompany: "Đại Học Bách Khoa",
-//        taxCode: "1111",
-//        address: "157B, đường Chùa Láng, Hà Nội",
-//        phoneNumber: "0854681997",
-//        email: "lqmanhddt@gmail.com"
-//    },
-//    {
-//        customerCode: "KH000003",
-//        customerName: "Lê Quốc Cường",
-//        customerCompany: "Đại Học Bách Khoa",
-//        taxCode: "1111",
-//        address: "157B, đường Chùa Láng, Hà Nội",
-//        phoneNumber: "0854681997",
-//        email: "lqmanhddt@gmail.com"
-//    },
-//    {
-//        customerCode: "KH000003",
-//        customerName: "Lê Quốc Hùng",
-//        customerCompany: "Đại Học Bách Khoa",
-//        taxCode: "1111",
-//        address: "157B, đường Chùa Láng, Hà Nội",
-//        phoneNumber: "0854681997",
-//        email: "lqmanhddt@gmail.com"
-
-//    }
-//];
